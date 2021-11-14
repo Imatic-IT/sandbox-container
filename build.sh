a88ec2bedb5a366462db58ce602b9ed092d12c78
Jan Pekař <jan.pekar@imatic.cz>
2021-11-14 21:11:56 +0100

    rename image name

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build . -t janpekar/testing-image
+docker build . -t janpekar/sandbox-container
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Start docker and pass public key to env variable, get container id
-cId=$(docker run -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d janpekar/testing-image)
+cId=$(docker run -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d janpekar/sandbox-container)
 #Inspect container with id and get its ip
 ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cId)
 #Ssh to that IP - disable strict host key checking because identity of container changes
