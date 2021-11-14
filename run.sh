5b6e8b783fb4315bb09ef5b13dc1667e38c828e4
Jan Pekař <jan.pekar@imatic.cz>
2021-11-14 21:22:12 +0100

    Allow multiple runs and connect to the same container, allow argument to create multiple instances

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#Start docker and pass public key to env variable, get container id
-cId=$(docker run -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d janpekar/sandbox-container)
+set -eu
+#You can specify arument to run multiple instances
+id="${1:-1}"
+containerName=sandbox-container-${id}
+imageName=janpekar/sandbox-container
+#Check if container is not running
+if [ ! "$(docker ps -q -f name=$containerName)" ]; then
+  #Start docker and pass public key to env variable
+  docker run -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d --name $containerName $imageName
+fi
 #Inspect container with id and get its ip
-ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cId)
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $containerName)
 #Ssh to that IP - disable strict host key checking because identity of container changes
 ssh -o StrictHostKeyChecking=no -o ForwardAgent=yes root@$ip
