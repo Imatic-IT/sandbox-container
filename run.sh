18ebf1d76012d2f917aa8d994fb6df4668404f7b
Jan Pekař <jan.pekar@imatic.cz>
2021-12-12 15:43:24 +0100

    Docker in docker run script

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ set -eu
 id="${1:-1}"
 containerName=sandbox-container-${id}
 imageName=janpekar/sandbox-container
+VOLUMES+="-v /var/run/docker.sock:/var/run/docker.sock "
+VOLUMES+="-v /usr/bin/docker:/usr/bin/docker:ro "
 #Check if container is not running
 if [ ! "$(docker ps -q -f name=$containerName)" ]; then
   #Start docker and pass public key to env variable
-  docker run -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d --name $containerName $imageName
+  docker run $VOLUMES -e AUTHORIZED_KEY_ROOT="$(ssh-add -L)" -d --name $containerName $imageName
   #Wait until container is started
   sleep 1
 fi
